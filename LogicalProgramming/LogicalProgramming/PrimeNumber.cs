100b9b483e77abf779c7b648748310afdb57a8ce
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-08 23:15:49 +0530

    [amol]- Check the given Number is Prime or Not

---
+++ LogicalProgramming/LogicalProgramming/PrimeNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class PrimeNumber
+    {
+        public void CheckPrime()
+        {
+            int  m = 0, flag = 0;
+            Console.WriteLine("Enter the Number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            m = n / 2;
+
+            for (int i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("Number is Not Prime");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("Number is Prime");
+            }
+            {
+
+            }
+        }
+    }
+}
--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+using LogicalProgramming;
+
+while (true)
+{
+    Console.WriteLine("1.Eter the Prime Number");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            PrimeNumber primeNumber = new PrimeNumber();
+            primeNumber.CheckPrime();
+            break;
+        default:
+            Console.WriteLine("Please Enter the Valid Choice");
+            break;
+    }
+}
