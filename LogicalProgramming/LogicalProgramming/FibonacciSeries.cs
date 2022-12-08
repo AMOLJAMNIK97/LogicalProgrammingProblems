5e2b01ea8fcdc2b868c73c329b38b2cc0bd02ff4
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-08 23:49:48 +0530

    [amol]- Fabonacci Series

---
+++ LogicalProgramming/LogicalProgramming/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class FibonacciSeries
+    {
+        public void CheckFibonacciSeries()
+        {
+            int n1 = 0, n2 = 1, n3, i;
+            Console.WriteLine("Enter the number of elements: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n1 + " " + n2 + " "); //printing 0 and 1    
+            for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+
+    }
+}
--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -5,13 +5,17 @@ using LogicalProgramming;
 
 while (true)
 {
-    Console.WriteLine("1.Eter the Prime Number");
+    Console.WriteLine("1.Prime Number\n 2. Fibonacci Series");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
         case 1:
             PrimeNumber primeNumber = new PrimeNumber();
             primeNumber.CheckPrime();
+            break;
+        case 2:
+            FibonacciSeries fibonacci = new FibonacciSeries();
+            fibonacci.CheckFibonacciSeries();
             break;
         default:
             Console.WriteLine("Please Enter the Valid Choice");
